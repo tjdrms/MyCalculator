7c47c62f9e7b25b22e021a2125cd428b4670a8b1
tjdrms <tjdrms0116>
2022-03-02 14:20:32 +0900

    간단한 계산기 어플리케이션

---
+++ app/src/main/java/com/example/myapplication/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.myapplication.dao.HistoryDao
+import com.example.myapplication.model.History
+
+@Database(entities = [History::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun historyDao() : HistoryDao
+}
---
+++ app/src/main/java/com/example/myapplication/dao/HistoryDao.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.myapplication.model.History
+
+@Dao
+interface HistoryDao {
+
+    @Query("SELECT * FROM history")
+    fun getAll(): List<History>
+
+    @Insert
+    fun insertHistory(history: History)
+
+    @Query("DELETE FROM history")
+    fun deleteAll()
+
+    //@Delete
+    //fun delete(history: History)
+
+    //@Query("SELECT * FROM history WHERE result LIKE :result LIMIT 1")
+    //fun finByResult(result : String): History
+}
